145bc1b9132bd01080b45a3a6aad160f155215bf
PruthvinathR <dhannapuneni@outlook.com>
2024-09-23 11:05:41 -0400

    Show only two clients.

--- frontend/src/app/clients/page.tsx
+++ frontend/src/app/clients/page.tsx
@@ -21,12 +21,6 @@ const ClientsPage = (props: Props) => {
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
         <ClientLink href="/categories" clientName="Client 1" clientId="1" />
         <ClientLink href="/categories" clientName="Client 2" clientId="2" />
-        <ClientLink href="/categories" clientName="Client 3" clientId="3" />
-        <ClientLink href="/categories" clientName="Client 4" clientId="4" />
-        <ClientLink href="/categories" clientName="Client 5" clientId="5" />
-        <ClientLink href="/categories" clientName="Client 6" clientId="6" />
-        <ClientLink href="/categories" clientName="Client 7" clientId="7" />
-        <ClientLink href="/categories" clientName="Client 8" clientId="8" />
     </div>
   )
 }
